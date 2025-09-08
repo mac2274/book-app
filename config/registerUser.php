f32b33662de0197bfd1da03b25229afe5306f4b9
Maggi <yuen.maggi@gmail.com>
2025-09-08 22:12:26 +0200

    commmit

--- config/registerUser.php
+++ config/registerUser.php
@@ -2,11 +2,13 @@
 
 require_once 'config.db.php';
 
-if (isset($_SERVER['REQUEST_METHOD'])==='POST' && isset($_POST['submitRegisterUSer'])){
-    $name = $_POST['name'];
-    $surname= $ $_POST['surname'];
-    $email = $_POST['email'];
-    $password= $ $_POST['pwd'];
-    
-    registerUser();
+if (isset($_SERVER['REQUEST_METHOD']) === 'POST' && isset($_POST['submitRegisterUSer'])){
+    $name = $_POST['name'] ?? '';
+    $email = $_POST['email'] ?? '';
+
+    if ($name && $email){
+        $password = $_POST['pwd'];
+    }
+
+    registerUser($name, $surname, $email, $hashedPassword);
 };
