0aa10fc2f6167a56dee2e9985231fca286faa34c
Maggi <yuen.maggi@gmail.com>
2026-03-31 11:32:05 +0200

    commit changes

--- php/search_favBookId.php
+++ php/search_favBookId.php
@@ -3,7 +3,18 @@
 require_once '../config/config.db.php';
 require_once '../config/lib.php';
 
-$result = $mysqli->query("SELECT id FROM books_fav ORDER BY id ASC");
-while ($row = $result->fetch_assoc()) {
+$stmt = $pdo->query("SELECT id FROM books_fav ORDER BY id ASC");
+foreach ($stmt->fetchAll() as $row) {
     echo $row['id'] . "<br>";
 }
+
+
+// <?php
+// // Test ob id gefunden wird!
+// require_once '../config/config.db.php';
+// require_once '../config/lib.php';
+
+// $result = $mysqli->query("SELECT id FROM books_fav ORDER BY id ASC");
+// while ($row = $result->fetch_assoc()) {
+//     echo $row['id'] . "<br>";
+// }
